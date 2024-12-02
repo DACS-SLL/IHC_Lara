0b3ea14b79ec122152a3e4119b7f7d52e1967c8a
DACS <dcasass@ulasalle.edu.pe>
2024-12-01 21:18:46 -0500

    New

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use  App\Http\Controllers\sesionCt;
 use App\Http\Controllers\LoginController;
 
 
-Route::get('/', sesionCt::class);
+Route::get('/sesion', sesionCt::class);
 
 Route::get('/plantillas', [sesionCt::class, "showPlantillas"])->name('plantillas');
 
@@ -16,7 +16,7 @@ Route::get('/multimedia', [sesionCt::class, "showMultimedia"])->name('multimedia
 
 Route::get('/home', [HomeCt::class, 'index'])->name('home');
 
-Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/', [LoginController::class, 'login'])->name('login');
 Route::post('/login', [LoginController::class, 'processLogin'])->name('processLogin');
 Route::get('/menu', [LoginController::class, 'menuPrincipal'])->name('menu');
 
