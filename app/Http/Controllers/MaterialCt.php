25c3bdc7c7959a5c5aa09d046d9eb6295fde2245
DACS <dcasass@ulasalle.edu.pe>
2024-12-02 23:55:49 -0500

    Agregar bilbioteca

---
+++ app/Http/Controllers/MaterialCt.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MaterialCt extends Controller
+{
+    public function create()
+    {
+        return view('agregarMat');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'titulo' => 'required|string|max:255',
+            'archivo' => 'required|file|mimes:pdf|max:2048' // Solo archivos PDF, máximo 2 MB
+        ]);
+
+        // Almacenar el archivo subido en la carpeta 'public/material'
+        $path = $request->file('archivo')->store('material', 'public');
+
+        return back()->with('success', 'Material agregado exitosamente.');
+    }
+}
